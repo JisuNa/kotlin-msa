61aab24e95d91018b978bc815e352db7ba7a5f3e
jsna <kuu2002@naver.com>
2019-04-21 01:46:38 +0900

    2장 스프링부트 2.0 시작하기

--- src/main/kotlin/com/microservices/ch02/Ch02Application.kt
+++ src/main/kotlin/com/microservices/ch02/Ch02Application.kt
@@ -1,11 +1,40 @@
 package com.microservices.ch02
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
-class Ch02Application
+class Ch02Application {
+    @Bean
+    @ConditionalOnExpression("#{'\${service.message.type}' == 'simple'}")
+    fun exampleService() : ServiceInterface = ExampleService()
+
+    @Bean
+    @ConditionalOnExpression("#{'\${service.message.type}' == 'advance'}")
+    fun advanceService() : ServiceInterface = AdvanceService()
+}
+
+@Controller
+class FirstController() {
+
+    @Autowired
+    lateinit var service : ServiceInterface
+
+    @RequestMapping(method = arrayOf(RequestMethod.GET), value = ["/user/{name}"])
+    @ResponseBody
+    fun hello(@PathVariable name: String)=service.getHello(name)
+}
 
 fun main(args: Array<String>) {
     runApplication<Ch02Application>(*args)
 }
+
+
